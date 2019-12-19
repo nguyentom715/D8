ca8f05dfe0bebe815e4667b3b0e2ac1964d670f3
nguyentom715 <nguyentom715@gmail.com>
2019-12-18 18:50:17 -0800

    Add files via upload

---
+++ blog_import/src/Form/BlogSettingsForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\blog_import\Form;
+
+/**
+ * Base handler for blog forms.
+ *
+ * @inheritdoc
+ */
+class BlogSettingsForm {
+
+    public function getFormId() {
+    }
+  
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(array $form, FormStateInterface $form_state) {
+    }
+  
+    /**
+     * {@inheritdoc}
+     */
+    public function validateForm(array &$form, FormStateInterface $form_state) {
+    }
+  
+    /**
+     * {@inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+    }
+}
